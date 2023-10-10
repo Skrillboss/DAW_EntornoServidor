aacd7a51eae12cc12a3c6e615f9a7b49ee4de40a
Luis Heredia <heredialuis20@gmail.com>
2023-10-10 16:54:44 +0200

    agragar los include_once

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 </head>
 
 <body>
+
     <form method="POST" action="publicar.php">
 
         <?php
--- menu.php
+++ menu.php
@@ -2,8 +2,10 @@
 <a href="login/logout.php" id="cerrarSesion">Cerrar sesion</a>
 
 
-
-<?php include_once "cabecera.html" ?>
+<?php include_once "cabecera.html"?>
 <?php include_once "modelo/entidades/publicacion.php" ?>
 <?php include_once "modelo/servicios/servicioPublicaciones.php" ?>
+<?php include_once "login/autenticacion.php"?>
+<?php include_once "modelo/servicios/servicioAutenticacion.php" ?>
+<?php Autenticacion::obtenerNombreUsuario();?>
 <?php session_start();?>
