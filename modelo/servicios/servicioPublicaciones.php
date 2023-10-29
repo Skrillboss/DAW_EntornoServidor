fa8e45d165232b9886c6dcdf7b8b3e174634417c
Luis Heredia <heredialuis20@gmail.com>
2023-10-29 12:22:50 +0100

    conexion a la base de datos completa

--- modelo/servicios/servicioPublicaciones.php
+++ modelo/servicios/servicioPublicaciones.php
@@ -2,27 +2,32 @@
 
     class servicioPublicaciones
     {
-
-        private static function inicializacion()
-        {
-
-            if (!isset($_SESSION["publicaciones"])) {
-                $_SESSION["publicaciones"] = array();
-            }
-        }
-
+    
         public static function obtenerPublicacion()
         {
+            $resultado = MySqlBd::consultaLectura("SELECT * FROM mensaje");
 
-            self::inicializacion();
-            return $_SESSION["publicaciones"];
+            $retorno = array();
+
+            foreach ($resultado as $publicacion) {
+                $dePago = $publicacion["depago"] ? "usuarioPago" : "usuarioNormal";
+                $fecha = new DateTime($publicacion["fecha"]);
+
+                $publicaciones = new publicacion($publicacion["nombre"], $dePago, $publicacion["texto"], $fecha);
+
+                array_push($retorno, $publicaciones);
+            }
+            return $retorno;
         }
 
         public static function insertarPublicacion($publicacion)
         {
-            self::inicializacion();
+            $usuario = $publicacion->usuario == "depago" ? 1 : 0;
+            $fecha = $publicacion->fecha->format("c");
+            $consulta = "INSERT INTO mensaje (nombre, depago, texto, fecha)
+            VALUES (?, ?, ?, ?)";
 
-            return array_push($_SESSION["publicaciones"], $publicacion);
+            MySqlBd::consultaEscritura($consulta, $publicacion->nombre, $usuario, $publicacion->texto, $fecha);
         }
     }
 
